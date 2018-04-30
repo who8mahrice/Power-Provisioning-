8d1aec13ac707c4b451ebb140cd9a883400b9c3a
who8mahrice <kvu4@uh.edu>
2018-04-30 11:08:39 -0500

    Update query.php

--- HOU2/Power Product/query.php
+++ HOU2/Power Product/query.php
@@ -1,117 +1,225 @@
 <?php
+session_start();
+
+//require_once 'navi.php';
+require_once 'newNav.php';
+//$customer = "";
+//$location = "";
 
 
 
 
-include 'query_functions.php';
+//include 'query_functions.php';
 //include 'css.css';
-require_once 'navi.php';
-
-
-
+//require_once 'navi.php';
 
 ?>
 
 
 <html>
- <head>
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Customer Query</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
-  <!-- For section popover  -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen"> 
+<link href="assets/datatables.min.css" rel="stylesheet" type="text/css">
 
- </head>
+<script type="text/javascript" src="assets/jquery-1.11.3-jquery.min.js"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+  /*
+  $("#btn-view").hide();
+  
+  $("#btn-add").click(function(){
+    $(".content-loader").fadeOut('slow', function()
+    {
+      $(".content-loader").fadeIn('slow');
+      $(".content-loader").load('add_form.php');
+      $("#btn-add").hide();
+      $("#btn-view").show();
+    });
+  });
+  
+
+  $("#btn-view").click(function(){
+    
+    $("body").fadeOut('slow', function()
+    {
+      $("body").load('index.php');
+      $("body").fadeIn('slow');
+      window.location.href="index.php";
+    });
+  });
+  */
+});
+</script>
+
+</head>
 
 
- <style>
-  table {
+<style>
+/* CSS STYLING IN HERE */ 
+#customerLocation option{
+            display:none;
+        }
+
+
+#customerLocation option.label{
+            display:block;
+}
+
+#customerLocationSession option{
+            display:none;
+        }
+
+
+#customerLocationSession option.label{
+            display:block;
+}
+
+
+</style>
+
+<body>
+<div class="container">
       
-    background-color: #eeeeee;
-    width:300px;
- }
+        <h2 class="form-signin-heading">Customer Records.</h2><hr />
+        <?php
+include 'connect.inc.php';
+        // echo "<div class='customer'>";
+        //         echo "<label for='customerName'>Customer:</label>";       
+        //         echo "<select  id='customerName' size='1' name='customerName'>";
+        //        echo "<option>-SELECT-</option>"; 
 
- th,td {
-    padding: 1px;
 
- }
 
- td{
-   
-    white-space:nowrap;  /*white-space:nowarp goes with <td width="100%"> Panel Name row to fit both drop down menus in one row */
+$connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+
+
+// START of checks connection
+if ($connection->connect_error) die($connection->connect_error);
+// END of checks connection
+
+//if($_SESSION['customer'] != ""){
+if(isset($_SESSION['customer']) && !empty($_SESSION['customer']) 
+  && isset($_SESSION['location']) && !empty($_SESSION['location']))
+{
+  echo "<div class='customer'>";
+  echo "<label for='customerNameSession'>Customer:</label>";       
+  echo "<select  id='customerNameSession' name='customerNameSession' value='".$_SESSION['customer']."''>";
+  echo "<option value='".$_SESSION['customer']."'>" .$_SESSION['customer']. '</option>'; // test
+  echo "<option>-SELECT-</option>"; 
+  $query = "SELECT * FROM spaceproduct";  
+  $result = mysqli_query($connection, $query);  
+  while($row = mysqli_fetch_array($result)){
+    echo "<option value='".$row['cID'] ."'>" .$row['cID']. '</option>';
+  }
+
+  echo'</select>';
+
+  echo "<select disabled='disabled' id='customerLocationSession' name='customerLocationSession' value='".$_SESSION['location']."'>";
+ echo "<option value='".$_SESSION['location']."'>" .$_SESSION['location']. '</option>'; // test
+  echo'<option>-SELECT-</option>'; // current one
+
+
+  $query = "SELECT * FROM spaceproduct";  
+  $result = mysqli_query($connection, $query);  
+  while($row = mysqli_fetch_array($result)){
+    echo "<option rel= ".$row['cID']."  value='".$row['location']."'>" .$row['location']. '</option>';
+                    //$customer = $row['cID'];
+                    //$location = $row['location'];
+  }
+
+  echo' </select>';
+
+debug_to_console($_SESSION['customer']."-Session_Name");
+debug_to_console($_SESSION['location']."-Session_Location");   
+        
+ session_unset(); 
+ session_destroy(); 
+
+debug_to_console($_SESSION['customer']);
+debug_to_console($_SESSION['location']);
+
+ 
+
+
+
+} else {
+
+  echo "<div class='customer'>";
+  echo "<label for='customerName'>Customer:</label>";       
+  echo "<select  id='customerName' size='1' name='customerName'>";
+  echo "<option>-SELECT-</option>"; 
+  $query = "SELECT * FROM spaceproduct";  
+  $result = mysqli_query($connection, $query);  
+  while($row = mysqli_fetch_array($result)){
+    echo "<option value='".$row['cID'] ."'>" .$row['cID']. '</option>';
+  }
+
+  echo'</select>';
+
+  echo'<select disabled="disabled" id="customerLocation" name="customerLocation">';
+  echo'<option value="">-SELECT-</option>';
+
+
+  $query = "SELECT * FROM spaceproduct";  
+  $result = mysqli_query($connection, $query);  
+  while($row = mysqli_fetch_array($result)){
+    echo "<option rel= ".$row['cID']."  value='".$row['location']."'>" .$row['location'].'</option>';
+  }
+ // end of else
+
+echo' </select>';
 }
 
-.customer{
-	 
-    width: 313px;
-    height: 300px;
-     position: absolute; 
+function debug_to_console( $data ) {
+    $output = $data;
+    // if ( is_array( $output ) )
+    //     $output = implode( ',', $output);
 
-    display: inline-block; 
-}
 
-.outputPrimaryCustomer table{
-	width: 500px;
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+      }
+?>
 
-}
-.outputPrimaryCustomer {
-	
-    float: left;
-    padding: 340px 0px 0px 0px; /* top right bottom left */
-    position: relative;
-  /*   background-color: blue; */
-    display: inline-block;
+                <!-- <input type='hidden' name="cID" id="cID" value=""> -->
+            </div>
+            <hr />
+            <div id='test'> </div>
+
+          </div>
+        </tbody>
+        </table>
+        
+        </div>
+
+    </div>
     
-  
-
-}
-
-.wholeOutput{
-	float: right;
-	position: absolute;
-	display: inline-block;
-/*	background-color: red; */
-}
-
-.outputSecondaryCustomer table{
-	width: 500px;
-
-}
-.outputSecondaryCustomer {
-	
-    float: right;
-    padding: 340px 0px 0px 0px; /* top right bottom left */
-    position: absolute;
-    display: inline-block;
-     /* background-color: yellow; */
+    <br />
     
-    
-  
+    <div class="container">
 
-}
 
- </style>
- <body>
-     <section id="content">
-     <div><h2> Query Customer </h2></div>
-     	<div class="customer">
-	     <form action = 'query.php' method='post'>
-	     	<table>
-             
-                <tr><td>Customer </td>
-                <td><input type='text' name='customer'></td></tr>
-                <tr><td>Location </td>
-                <td><input type='text' name='location'></td></tr>
-                    <td>sIDs </td><td>
-                	<textarea style="resize:none" name="searchsID" rows="10" cols="6" class="searchsID" placeholder="sIDs Here" ></textarea>
-                </td></tr>
-                <tr><td><td><input type="submit" value="Query Customer" align="center" name="queryCustomer"></td></td></tr>
-             </table>
-	     </form>
-	    </div>
-	    <div class="output">
-	    </div> 
-     </section>
- </body>
+    </div>
+ 
+
+
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/datatables.min.js"></script>
+<script type="text/javascript" src="js/query.js"></script>
+
+<script type="text/javascript" charset="utf-8">
+$(document).ready(function() {
+  $('#example').DataTable();
+
+  $('#example')
+  .removeClass( 'display' )
+  .addClass('table table-bordered');
+});
+</script>
+</body>
+</html>
 
